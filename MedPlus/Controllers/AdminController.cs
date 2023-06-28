6d429923353a25ee5a70af5453baea1db6e61f37
Ramakrishna <pramakrishna49@gmail.com>
2023-06-29 00:23:36 +0530

    Added Admin controller added

---
+++ MedPlus/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MedPlus.Controllers
+{
+    public class AdminController : ApiController
+    {
+
+        //Add admin
+
+        //Login admin
+
+        //Update admin
+    }
+}
